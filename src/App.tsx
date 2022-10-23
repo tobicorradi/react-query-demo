7d32c425f68ae4bf08b5dd7c5c459ea488d0c8a1
Tobias Corradi <corraditobias@gmail.come>
2022-10-23 09:52:49 -0300

    PhotoGallery component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import PhotoGallery from './components/PhotoGallery'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       Photolist API + React Query
+      <PhotoGallery />
     </div>
   )
 }
